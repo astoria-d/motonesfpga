034d054ffc176fcabdd9e4e4646e26c069b9917b
astoria-d <astoria-d@mail.goo.ne.jp>
2013-05-20 15:51:03 +0900

    prg rom created.

---
+++ simulation/mem/prg_rom.vhd
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.conv_integer;
+
+entity prg_rom is 
+    generic (addr_size : integer := 16; data_size : integer := 8);
+    port (  clk, ce         : in std_logic;
+            addr            : in std_logic_vector (addr_size - 1 downto 0);
+            data            : out std_logic_vector (data_size - 1 downto 0)
+        );
+end prg_rom;
+
+architecture rtl of prg_rom is
+
+subtype rom_data is std_logic_vector (data_size -1 downto 0);
+type rom_array is array (0 to 2**addr_size) of rom_data;
+
+constant p_rom : rom_array := rom_array'(
+        x"01",
+        x"02",
+        x"03",
+        x"04",
+        x"a1",
+        x"b1",
+        x"c1",
+        x"d1",
+        x"ff",
+        x"aa",
+        x"11",
+        others=>x"00"
+        );
+
+begin
+    p : process (clk, ce, addr)
+    begin
+    if (clk'event and clk = '1') then
+        if (ce = '1') then
+           data <= p_rom(conv_integer(addr));
+        end if;
+    end if;
+    end process;
+end rtl;
+
---
+++ simulation/mem/testbench_prg_rom.vhd
@@ -0,0 +1,71 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use std.textio.all;
+
+entity testbench_prg_rom is
+end testbench_prg_rom;
+
+architecture stimulus of testbench_prg_rom is 
+    component prg_rom
+        generic (addr_size : integer := 16; data_size : integer := 8);
+        port (  clk, ce         : in std_logic;
+                addr            : in std_logic_vector (addr_size - 1 downto 0);
+                data            : out std_logic_vector (data_size - 1 downto 0)
+            );
+    end component;
+    constant interval : time := 15 ns;
+    constant dsize : integer := 8;
+    --constant asize : integer := 16#8000#;
+    constant asize : integer := 15;
+    signal cclk, cce : std_logic;
+    signal aa       : std_logic_vector (asize - 1 downto 0);
+    signal dd       : std_logic_vector (dsize - 1 downto 0);
+begin
+    dut0 : prg_rom generic map (asize, dsize) port map (cclk, cce, aa, dd);
+
+    p1 : process
+    variable i : integer := 0;
+    constant loopcnt : integer := 10;
+    begin
+
+        for i in 0 to loopcnt * 2 loop
+            cclk <= '1';
+            wait for interval / 2;
+            cclk <= '0';
+            wait for interval / 2;
+        end loop;
+    end process;
+
+    p2 : process
+    variable i : integer := 0;
+    constant loopcnt : integer := 5;
+    begin
+
+        wait for interval / 4;
+
+        for i in 0 to loopcnt loop
+            aa <= conv_std_logic_vector(i, asize);
+            wait for interval / 4;
+        end loop;
+
+        cce <= '1';
+
+        for i in 0 to loopcnt * 2 loop
+            aa <= conv_std_logic_vector(i, asize);
+            wait for interval / 2;
+        end loop;
+
+        cce <= '0';
+
+        for i in 0 to loopcnt * 2 loop
+            aa <= conv_std_logic_vector(i, asize);
+            wait for interval / 2;
+        end loop;
+
+        wait;
+    end process;
+
+end stimulus ;
+
