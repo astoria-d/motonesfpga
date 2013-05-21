5b588f2c20b685bc96c3e47a3a56f260a6610829
astoria-d <astoria-d@mail.goo.ne.jp>
2013-05-21 13:19:59 +0900

    makefile and code updates.

--- simulation/cpu/regn.vhd
+++ simulation/cpu/regn.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity regn is 
     generic (dsize : integer := 8);
     port (  clk, en     : in std_logic;
-            r           : in std_logic_vector (dsize - 1 downto 0);
+            d           : in std_logic_vector (dsize - 1 downto 0);
             q           : out std_logic_vector (dsize - 1 downto 0)
         );
 end regn;
@@ -15,7 +15,7 @@ begin
     begin
     if (clk'event and clk = '1') then
         if (en = '1') then
-            q <= r;
+            q <= d;
         end if;
     end if;
     end process;
--- simulation/cpu/testbench_regn.vhd
+++ simulation/cpu/testbench_regn.vhd
@@ -13,7 +13,7 @@ architecture stimulus of testbench_regn is
     component regn
         generic (dsize : integer := 8);
         port (  clk, en     : in std_logic;
-                r           : in std_logic_vector (dsize - 1 downto 0);
+                d           : in std_logic_vector (dsize - 1 downto 0);
                 q           : out std_logic_vector (dsize - 1 downto 0)
             );
     end component;
@@ -22,13 +22,13 @@ architecture stimulus of testbench_regn is
     constant dsize8 : integer := 8;
     constant dsize16 : integer := 16;
     signal cclk, een : std_logic;
-    signal rr1, qq1    : std_logic_vector (dsize1 - 1 downto 0);
-    signal rr8, qq8    : std_logic_vector (dsize8 - 1 downto 0);
-    signal rr16, qq16    : std_logic_vector (dsize16 - 1 downto 0);
+    signal dd1, qq1    : std_logic_vector (dsize1 - 1 downto 0);
+    signal dd8, qq8    : std_logic_vector (dsize8 - 1 downto 0);
+    signal dd16, qq16    : std_logic_vector (dsize16 - 1 downto 0);
 begin
-    dut0 : regn generic map (dsize1) port map (cclk, een, rr1, qq1);
-    dut1 : regn generic map (dsize8) port map (cclk, een, rr8, qq8);
-    dut2 : regn generic map (dsize16) port map (cclk, een, rr16, qq16);
+    dut0 : regn generic map (dsize1) port map (cclk, een, dd1, qq1);
+    dut1 : regn generic map (dsize8) port map (cclk, een, dd8, qq8);
+    dut2 : regn generic map (dsize16) port map (cclk, een, dd16, qq16);
 
     p1 : process
     variable i : integer := 0;
@@ -51,55 +51,55 @@ begin
         wait for interval / 4;
 
         for i in 0 to loopcnt loop
-            rr1 <= conv_std_logic_vector(i, dsize1);
-            rr8 <= conv_std_logic_vector(i, dsize8);
-            rr16 <= conv_std_logic_vector(i, dsize16);
+            dd1 <= conv_std_logic_vector(i, dsize1);
+            dd8 <= conv_std_logic_vector(i, dsize8);
+            dd16 <= conv_std_logic_vector(i, dsize16);
             wait for interval / 4;
         end loop;
 
         een <= '1';
 
         for i in 0 to loopcnt * 3 loop
-            rr1 <= conv_std_logic_vector(i, dsize1);
-            rr8 <= conv_std_logic_vector(i, dsize8);
-            rr16 <= conv_std_logic_vector(i, dsize16);
+            dd1 <= conv_std_logic_vector(i, dsize1);
+            dd8 <= conv_std_logic_vector(i, dsize8);
+            dd16 <= conv_std_logic_vector(i, dsize16);
             wait for interval / 3;
         end loop;
 
         for i in 0 to loopcnt * 4 loop
-            rr1 <= conv_std_logic_vector(i, dsize1);
-            rr8 <= conv_std_logic_vector(i, dsize8);
-            rr16 <= conv_std_logic_vector(i, dsize16);
+            dd1 <= conv_std_logic_vector(i, dsize1);
+            dd8 <= conv_std_logic_vector(i, dsize8);
+            dd16 <= conv_std_logic_vector(i, dsize16);
             wait for interval / 4;
         end loop;
 
         for i in 0 to loopcnt * 5 loop
-            rr1 <= conv_std_logic_vector(i, dsize1);
-            rr8 <= conv_std_logic_vector(i, dsize8);
-            rr16 <= conv_std_logic_vector(i, dsize16);
+            dd1 <= conv_std_logic_vector(i, dsize1);
+            dd8 <= conv_std_logic_vector(i, dsize8);
+            dd16 <= conv_std_logic_vector(i, dsize16);
             wait for interval / 5;
         end loop;
 
         for i in 0 to loopcnt * 2 loop
-            rr1 <= conv_std_logic_vector(i, dsize1);
-            rr8 <= conv_std_logic_vector(i, dsize8);
-            rr16 <= conv_std_logic_vector(i, dsize16);
+            dd1 <= conv_std_logic_vector(i, dsize1);
+            dd8 <= conv_std_logic_vector(i, dsize8);
+            dd16 <= conv_std_logic_vector(i, dsize16);
             wait for interval;
         end loop;
         
         for i in 0 to loopcnt * 2 loop
-            rr1 <= conv_std_logic_vector(i, dsize1);
-            rr8 <= conv_std_logic_vector(i, dsize8);
-            rr16 <= conv_std_logic_vector(i, dsize16);
+            dd1 <= conv_std_logic_vector(i, dsize1);
+            dd8 <= conv_std_logic_vector(i, dsize8);
+            dd16 <= conv_std_logic_vector(i, dsize16);
             wait for interval * 2;
         end loop;
 
         een <= '0';
 
         for i in 0 to loopcnt * 3 loop
-            rr1 <= conv_std_logic_vector(i, dsize1);
-            rr8 <= conv_std_logic_vector(i, dsize8);
-            rr16 <= conv_std_logic_vector(i, dsize16);
+            dd1 <= conv_std_logic_vector(i, dsize1);
+            dd8 <= conv_std_logic_vector(i, dsize8);
+            dd16 <= conv_std_logic_vector(i, dsize16);
             wait for interval / 3;
         end loop;
 
