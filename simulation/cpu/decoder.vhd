c5f07212a46a0b72268700e232750577e57a04d9
astoria-d <astoria-d@mail.goo.ne.jp>
2013-05-28 00:54:43 +0900

    reset initialize period updated. (still yet to be done)

--- simulation/cpu/decoder.vhd
+++ simulation/cpu/decoder.vhd
@@ -1,19 +1,68 @@
 library ieee;
 use ieee.std_logic_1164.all;
+--use ieee.std_logic_arith.all;
 
 entity decoder is 
     generic (dsize : integer := 8);
-    port (  trig_clk        : in std_logic;
+    port (  set_clk         : in std_logic;
+            trig_clk        : in std_logic;
+            res_n           : in std_logic;
+            irq_n           : in std_logic;
+            nmi_n           : in std_logic;
+            rdy             : in std_logic;
             instruction     : in std_logic_vector (dsize - 1 downto 0);
             status_reg      : in std_logic_vector (dsize - 1 downto 0);
             pcl_d_i_n       : out std_logic;
             pcl_d_o_n       : out std_logic;
-            pcl_a_o_n       : out std_logic
+            pcl_a_o_n       : out std_logic;
+            r_nw            : out std_logic
         );
 end decoder;
 
 architecture rtl of decoder is
 
+type dec_status is (reset0, reset1, reset2, reset3, reset4, reset5, fetch);
+
+signal cur_stat : dec_status;
+
 begin
+
+    main_p : process (set_clk, trig_clk, res_n)
+    begin
+        if (res_n'event and res_n = '0') then
+            cur_stat <= reset0;
+        else
+            if (set_clk'event and set_clk = '1') then
+                case cur_stat is
+                    when reset0 => 
+                        pcl_d_i_n <= '0';
+                    when fetch => 
+                    when others => null;
+                end case;
+            end if;
+
+            if (trig_clk'event and trig_clk = '1') then
+                case cur_stat is
+                    when reset0 => 
+                        pcl_d_i_n <= '1';
+                        cur_stat <= reset1;
+                    when reset1 => 
+                        cur_stat <= reset2;
+                    when reset2 => 
+                        cur_stat <= reset3;
+                    when reset3 => 
+                        cur_stat <= reset4;
+                    when reset4 => 
+                        cur_stat <= reset5;
+                    when reset5 => 
+                        cur_stat <= fetch;
+                    when fetch => 
+                        pcl_a_o_n <= '0';
+                    when others => null;
+                end case;
+            end if;
+        end if;
+    end process;
+
 end rtl;
 
--- simulation/cpu/mos6502.vhd
+++ simulation/cpu/mos6502.vhd
@@ -33,29 +33,61 @@ architecture rtl of mos6502 is
             );
     end component;
 
+    component decoder
+        generic (dsize : integer := 8);
+        port (  set_clk         : in std_logic;
+                trig_clk        : in std_logic;
+                res_n           : in std_logic;
+                irq_n           : in std_logic;
+                nmi_n           : in std_logic;
+                rdy             : in std_logic;
+                instruction     : in std_logic_vector (dsize - 1 downto 0);
+                status_reg      : in std_logic_vector (dsize - 1 downto 0);
+                pcl_d_i_n       : out std_logic;
+                pcl_d_o_n       : out std_logic;
+                pcl_a_o_n       : out std_logic;
+                r_nw            : out std_logic
+            );
+    end component;
+
+    signal set_clk : std_logic;
     signal trigger_clk : std_logic;
+
     signal pc_d_in_n : std_logic;
     signal pc_d_out_n : std_logic;
     signal pc_a_out_n : std_logic;
 
+    --internal bus (address hi/lo, data)
     signal internal_abus_h : std_logic_vector (dsize - 1 downto 0);
     signal internal_abus_l : std_logic_vector (dsize - 1 downto 0);
     signal internal_dbus : std_logic_vector (dsize - 1 downto 0);
 
+    signal instruction : std_logic_vector (dsize - 1 downto 0);
+    signal status_reg : std_logic_vector (dsize - 1 downto 0);
 begin
 
+    ---instances....
     pc_l : pc generic map (dsize) 
             port map(trigger_clk, pc_d_in_n, pc_d_out_n, pc_a_out_n, 
                     internal_dbus, internal_abus_l);
 
+    dec_inst : decoder generic map (dsize) 
+            port map(set_clk, trigger_clk, rst_n, irq_n, nmi_n, 
+                    rdy, instruction, status_reg,
+                    pc_d_in_n, pc_d_out_n, pc_a_out_n,
+                    r_nw
+                    );
+
     -- clock generate.
     phi1 <= input_clk;
     phi2 <= not input_clk;
+    set_clk <= input_clk;
     trigger_clk <= not input_clk;
 
     reset_p : process (rst_n)
     begin
         if (rst_n'event and rst_n = '0') then
+            internal_dbus <= "10000000"; --address 0x8000
 
         end if;
     end process;
