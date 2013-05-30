8decd0f6678b04b86dafb7a8ad84917e45a8517d
astoria-d <astoria-d@mail.goo.ne.jp>
2013-05-30 16:16:59 +0900

    update. partially merge from simple-arch.

--- simulation/cpu/decoder.vhd
+++ simulation/cpu/decoder.vhd
@@ -35,68 +35,64 @@ begin
 end  procedure;
 
 type dec_status is (reset0, reset1, reset2, reset3, reset4, reset5, 
-                    fetch, 
+                    fetch, exec, 
                     sei,
-                    nop);
+                    nop,
+                    unknown_stat
+                    );
 
+signal cur_stat : dec_status;
 
 begin
 
     main_p : process (set_clk, trig_clk, res_n)
-    variable cur_stat : dec_status;
     begin
         if (res_n'event and res_n = '0') then
-            cur_stat := reset0;
-        else
-            if (set_clk'event and set_clk = '1') then
-                ---one byte instruction decoding.
-                if instruction = conv_std_logic_vector(16#78#, dsize) then
-                    cur_stat := sei;
-                    --pc_inc_n <= '0';
-                end if;
+            cur_stat <= reset0;
+        end if;
 
-                case cur_stat is
-                    when reset0 => 
-                    when fetch => 
-                        pcl_a_o_n <= '0';
-                        pch_a_o_n <= '0';
-                        r_nw <= '1';
-                        d_print(string'("fetch 0"));
-                    when sei => 
+        if (set_clk'event and set_clk = '1') then
+            d_print(string'("set_clk"));
+
+            case cur_stat is
+                when reset0 => 
+                    cur_stat <= reset1;
+                when reset1 => 
+                    cur_stat <= reset2;
+                when reset2 => 
+                    cur_stat <= reset3;
+                when reset3 => 
+                    cur_stat <= reset4;
+                when reset4 => 
+                    cur_stat <= reset5;
+                when reset5 => 
+                    cur_stat <= fetch;
+                when fetch => 
+                    d_print(string'("fetch"));
+                    pcl_a_o_n <= '0';
+                    pch_a_o_n <= '0';
+                    r_nw <= '1';
+                    cur_stat <= exec;
+                when exec => 
+                    d_print(string'("exec and decode"));
+                    ---one byte instruction decoding.
+                    if instruction = conv_std_logic_vector(16#78#, dsize) then
+                        d_print(string'("sei"));
                         pcl_a_o_n <= '1';
                         pch_a_o_n <= '1';
-                        pc_inc_n <= '1';
-                        d_print(string'("sei 0"));
-                    when others => null;
-                end case;
-            end if;
-
-            if (trig_clk'event and trig_clk = '1') then
-
-                case cur_stat is
-                    when reset0 => 
-                        cur_stat := reset1;
-                    when reset1 => 
-                        cur_stat := reset2;
-                    when reset2 => 
-                        cur_stat := reset3;
-                    when reset3 => 
-                        cur_stat := reset4;
-                    when reset4 => 
-                        cur_stat := reset5;
-                    when reset5 => 
-                        cur_stat := fetch;
-                    when fetch => 
-                        d_print(string'("fetch 1"));
-                    when sei => 
-                        cur_stat := fetch;
-                        d_print(string'("sei 1"));
                         pc_inc_n <= '0';
-                    when others => 
-                        null;
-                end case;
-            end if;
+                        cur_stat <= fetch;
+                    else
+                        cur_stat <= unknown_stat;
+                        d_print(string'("unknown inst."));
+                    end if;
+
+                when others => null;
+                    d_print(string'("unknown status."));
+                    cur_stat <= unknown_stat;
+            end case;
         end if;
+
     end process;
 
 end rtl;
