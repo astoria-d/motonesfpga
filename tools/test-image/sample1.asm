720a1e8acf1389c68b660a57e398e054f02b4e1d
astoria-d <astoria-d@mail.goo.ne.jp>
2013-07-28 11:13:44 +0900

    test env update.

--- tools/test-image/sample1.asm
+++ tools/test-image/sample1.asm
@@ -1,11 +1,7 @@
-;----------------------------------------------------------------------------
-;				NES Sample1 "HELLO, WORLD!"
-;					Copyright (C) 2007, Tekepen
-;----------------------------------------------------------------------------
 .setcpu		"6502"
 .autoimport	on
 
-; iNESヘッダ
+; iNES header
 .segment "HEADER"
 	.byte	$4E, $45, $53, $1A	; "NES" Header
 	.byte	$02			; PRG-BANKS
@@ -16,24 +12,25 @@
 	.byte	$00, $00, $00, $00	; 
 
 .segment "STARTUP"
-; リセット割り込み
 .proc	Reset
+; interrupt off, initialize sp.
 	sei
 	ldx	#$ff
 	txs
 
-; スクリーンオフ
+    ;ppu register initialize.
 	lda	#$00
 	sta	$2000
 	sta	$2001
 
-; パレットテーブルへ転送(BG用のみ転送)
 	lda	#$3f
 	sta	$2006
 	lda	#$00
 	sta	$2006
 	ldx	#$00
 	ldy	#$10
+
+    ;;load palette.
 copypal:
 	lda	palettes, x
 	sta	$2007
@@ -41,13 +38,14 @@ copypal:
 	dey
 	bne	copypal
 
-; ネームテーブルへ転送(画面の中央付近)
 	lda	#$21
 	sta	$2006
 	lda	#$c9
 	sta	$2006
 	ldx	#$00
-	ldy	#$0d		; 13文字表示
+	ldy	#$0d
+
+    ;;load name table.
 copymap:
 	lda	string, x
 	sta	$2007
@@ -55,38 +53,35 @@ copymap:
 	dey
 	bne	copymap
 
-; スクロール設定
 	lda	#$00
 	sta	$2005
 	sta	$2005
 
-; スクリーンオン
+    ;;show test msg.
 	lda	#$08
 	sta	$2000
 	lda	#$1e
 	sta	$2001
 
-; 無限ループ
+    ;;infinite loop.
 mainloop:
 	jmp	mainloop
 .endproc
 
-; パレットテーブル
 palettes:
 	.byte	$0f, $00, $10, $20
 	.byte	$0f, $06, $16, $26
 	.byte	$0f, $08, $18, $28
 	.byte	$0f, $0a, $1a, $2a
 
-; 表示文字列
 string:
-	.byte	"MOTO, mootoo!"
+	.byte	"test2!"
 
 .segment "VECINFO"
 	.word	$0000
 	.word	Reset
 	.word	$0000
 
-; パターンテーブル
+; character rom file.
 .segment "CHARS"
 	.incbin	"character.chr"
