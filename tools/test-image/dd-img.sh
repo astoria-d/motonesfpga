0ea3d82837f0f8164899614fc2ca1aa6bab7f3de
astoria-d <astoria-d@mail.goo.ne.jp>
2013-09-23 18:48:49 +0900

    update

--- tools/test-image/dd-img.sh
+++ tools/test-image/dd-img.sh
@@ -19,5 +19,7 @@ echo "processing...."
 
 dd if=$in_file of=$out_file1 bs=16 skip=1 count=2048 2> /dev/null
 dd if=$in_file of=$out_file2 bs=16 skip=2049 2> /dev/null
+#4k img creation
+#dd if=sample1-prg.bin of=sample1-prg-4k.bin bs=512 count=8
 
 echo "done."
