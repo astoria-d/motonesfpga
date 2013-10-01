f202294dbb6c52ceed6fbffd89075cc04d961cfd
astoria-d <astoria-d@mail.goo.ne.jp>
2013-10-01 17:48:02 +0900

    regression test working...

--- tools/regression-test/regression.asm
+++ tools/regression-test/regression.asm
@@ -11,179 +11,84 @@
 	.byte	$00, $00, $00, $00	; 
 	.byte	$00, $00, $00, $00	; 
 
+
+;;;;memory map
+;;0000 - 00ff  zp: volatile though functions
+;;0100 - 01ff  sp: preserved through functions   
+;;0200 - 02ff  dma: dedicated for dma
+;;0300 - 07ff  global: global variables.
+
 .segment "STARTUP"
 .proc	Reset
-
-;;; de1 env decoder bug test
-;;;LDA   $8182, y
-;;;STA   $2007
-;;;INY   
-;;;DEX   
-;;;;;BPL   #-10
-;;;LDA   $8182, y
-;;;STA   $2007
-;;;INY   
-;;;DEX   
-;;;;;BPL   #-10
-;;;LDA   #$3d
-;;;STA   $0302               ;;;>>>invalid store address!!!! @ 907,921,200 ps
-
-
-
 
 ; interrupt off, initialize sp.
 	sei
 	ldx	#$ff
 	txs
 
-    ;ppu register initialize.
-	lda	#$00
-	sta	$2000
-	sta	$2001
+    jsr init_ppu
+    jsr init_global
 
+    lda ad_start_msg
+    sta $00
+    lda ad_start_msg+1
+    sta $01
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
 
-	lda	#$3f
-	sta	$2006
-	lda	#$00
-	sta	$2006
+    lda ad_tes_msg2
+    sta $00
+    lda ad_tes_msg2+1
+    sta $01
+    jsr print_ln
 
-    ;;load palette.
-	ldx	#$00
-	ldy	#$20
-copypal:
-	lda	palettes, x
-	sta	$2007
-	inx
-	dey
-	bne	copypal
+    lda ad_tes_msg3
+    sta $00
+    lda ad_tes_msg3+1
+    sta $01
+    jsr print_ln
 
-	lda	#$20
-	sta	$2006
-	lda	#$ab
-	sta	$2006
-	ldx	#$00
-	ldy	#$0d
+    lda ad_tes_msg2
+    sta $00
+    lda ad_tes_msg2+1
+    sta $01
+    jsr print_ln
+    jsr print_ln
+    jsr print_ln
 
-    ;;load name table.
-copymap:
-	lda	string, x
-	sta	$2007
-	inx
-	dey
-	bne	copymap
+    lda ad_tes_msg3
+    sta $00
+    lda ad_tes_msg3+1
+    sta $01
+    jsr print_ln
 
-    ;;scroll reg set.
-	lda	#$00
-	sta	$2005
-	sta	$2005
-
-;;;;----------------------
-    ;;load name tbl.
-    ldy #$00
-    ldx #$40    ;;name table entry cnt.
-
-    lda #$20
-    sta $2006
-    lda #$c0
-    sta $2006
-
-nt_st:
-    lda nt1, y
-    sta $2007
-    iny
-    dex
-    bpl nt_st
-
-    ;;load attr tbl.
-    ldy #$00
-    ldx #$08    ;;attribute entry cnt
-
-    lda #$23
-    sta $2006
-    lda #$c8
-    sta $2006
-
-at_st:
-    lda at1, y
-    sta $2007
-    iny
-    dex
-    bpl at_st
-
-    ;;set universal bg color.
-    lda #$3d
-    sta $0302
-    jsr set_bg_col
-
-    ;;set scroll reg.
-    ;;lda #$a6
-    lda #$05
-    sta $0300
-    lda #00
-    sta $0301
-    jsr set_scroll
-
-    ;;set next page name table
-    ldy #$00
-    ldx #$0b
-
-    lda #$24
-    sta $2006
-    lda #$c0
-    sta $2006
-
-nt2_st:
-    lda nt2, y
-    sta $2007
-    iny
-    dex
-    bpl nt2_st
-
-    ;;next page attr.
-    lda #$27
-    sta $2006
-    lda #$d0
-    sta $2006
-
-    lda #$e4
-    sta $2007
-
-;;;    ;;dma test data.
-;;;    ldy #$00
-;;;    ldx #$41
-;;;    stx $00
-;;;    ldx #$00
-;;;dma_set:
-;;;    ;;y pos
-;;;    txa
-;;;    sta $0200, y
-;;;    iny
-;;;    ;;tile index
-;;;    lda $00
-;;;    cmp #$5b
-;;;    bne inc_tile
-;;;    lda #$41
-;;;    sta $00
-;;;inc_tile:
-;;;    inc $00
-;;;    sta $0200, y
-;;;    iny
-;;;    ;;attribute
-;;;    lda #$01
-;;;    sta $0200, y
-;;;    iny
-;;;    ;;x pos
-;;;    txa
-;;;    adc #$03
-;;;    tax
-;;;    rol
-;;;    sta $0200, y
-;;;    iny
-;;;    bne dma_set
-;;;
-;;;    ;;dma start.
-;;;    lda #$02
-;;;    sta $4014
 
     ;;show bg...
 	lda	#$1e
@@ -196,101 +101,107 @@ nt2_st:
     ;;done...
     ;;infinite loop.
 mainloop:
-
-    ;;read ppu status reg while displaying
-    ;;vram read test
-    ldx #$0a
-l1:
-    nop
-    dex
-    bne l1
-
-    ldx #$0a
-read_status:
-    lda $2002
-    dex
-    bne read_status
-
 	jmp	mainloop
 .endproc
 
 
 nmi_test:
-    jsr set_scroll
-    jsr set_bg_col
-
     rti
 
-add_nl:
-    clc
-    txa
-    pha
-
-    lda $01
+;;;param $00, $01 = msg addr.
+.proc print_ln
+    lda vram_current
+    sta $2006
+    lda vram_current + 1
     sta $2006
 
-    lda $00
-    adc #$20
-    sta $00
-    sta $2006
-
-    bcc no_carry
-    lda $01
-    adc #$00
-    sta $01
-    sta $2006
-    lda $00
-    sta $2006
-no_carry:
-
-    pla
-    tax
-    rts
-
-set_scroll:
-    lda $0300
-    sta $2005
-    clc
-    adc #$05
-    sta $0300
-    lda $0301
-    sta $2005
-    clc
-    adc #04
-;;    sta $0301
-    rts
-
-set_bg_col:
-    lda #$3f
-    sta $2006
-    lda #$10
-    sta $2006
-    lda $0302
+    ldy #$00
+@msg_loop:
+    lda ($00), y
     sta $2007
-    cmp #$30
-    bne bg_dec
-    lda #$3d
-    sta $0302
-    jmp bg_done
-bg_dec:
-    dec $0302
-bg_done:
+    beq @print_done
+    iny
+    jmp @msg_loop
+@print_done:
+
+    ;;clear remaining space.
+@clr_line:
+    tya
+    and #$1f
+    cmp #$1f
+    beq @clr_done
+    lda #$00
+    sta $2007
+    iny
+    jmp @clr_line
+@clr_done:
+
+    ;;renew vram pos
+    lda vram_current + 1
+    sty vram_current + 1
+    adc vram_current + 1
+    sta vram_current + 1
+    tax         ;; x = new vram_l
+    lda vram_current
+    bcc @no_carry
+    clc
+    adc #01     ;; a = new vram_h
+    sta vram_current
+@no_carry:
+
+    cmp #$23
+    bne @vpos_done
+
+    txa 
+    cmp #$c0
+    bne @vpos_done
+    ;;;if vram pos = 23c0. reset pos.
+    lda #$20
+    sta vram_current
+    lda #$00
+    sta vram_current + 1
+@vpos_done:
+
+    ;;scroll 1 line
+    lda scroll_x
+    sta $2005
+
+    lda scroll_y
+    clc
+    adc #8
+    cmp #240
+    bne @scr_done
+    lda #$0
+@scr_done:
+    sta scroll_y
+    sta $2005
+
+    rts
+.endproc
+
+.proc init_ppu
+    ;ppu register initialize.
+	lda	#$00
+	sta	$2000
+	sta	$2001
+
+    ;;load palette.
+	lda	#$3f
+	sta	$2006
+	lda	#$00
+	sta	$2006
+
+	ldx	#$00
+	ldy	#$20
+@copypal:
+	lda	@palettes, x
+	sta	$2007
+	inx
+	dey
+	bne	@copypal
     rts
 
-nt1:
-	.byte	$41, $42, $43, $44, $45, $46, $47, $48, $49, $4a, $4b, $4c, $4d, $4e, $4f, $50
-	.byte	$61, $62, $63, $64, $65, $66, $67, $68, $69, $6a, $6b, $6c, $6d, $6e, $6f, $70
-	.byte	$80, $81, $82, $83, $84, $85, $86, $87, $88, $89, $8a, $8b, $8c, $8d, $8e, $8f
-	.byte	$90, $91, $92, $93, $94, $95, $96, $97, $98, $99, $9a, $9b, $9c, $9d, $9e, $9f
-nt2:
-	.byte	$6b, $6a, $69, $68, $67, $66, $65, $64, $63, $62, $61
-	.byte	$30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $3a
-
-at1:
-	.byte	$1b, $e4, $a5, $5a
-	.byte	$e4, $1b, $5a, $a5
-
-palettes:
+@palettes:
 ;;;bg palette
 	.byte	$0f, $00, $10, $20
 	.byte	$0f, $04, $14, $24
@@ -302,8 +213,58 @@ palettes:
 	.byte	$0f, $08, $18, $28
 	.byte	$0f, $0a, $1a, $2a
 
-string:
-	.byte	"test2!"
+.endproc
+
+
+.proc init_global
+;;vram pos start from the bottom line.
+    lda #$23
+    sta vram_current
+    lda #$a0
+    sta vram_current + 1
+
+    lda #$00
+    sta scroll_x
+    lda #232
+    sta scroll_y
+    rts
+
+.endproc
+
+
+
+
+;;;;string datas
+ad_start_msg:
+    .addr   start_msg
+start_msg:
+    .byte   "regression test start..."
+    .byte   $00
+
+ad_tes_msg2:
+    .addr   tes_msg2
+tes_msg2:
+    .byte   "msg2"
+    .byte   $00
+
+ad_tes_msg3:
+    .addr   tes_msg3
+tes_msg3:
+    .byte   "msg3"
+    .byte   $00
+
+
+
+;;;;global variables.
+.segment "BSS"
+vram_current:
+    .byte   $00
+    .byte   $00
+scroll_x:
+    .byte   $00
+scroll_y:
+    .byte   $00
+
 
 ;;;for DE1 internal memory constraints.
 .segment "VECINFO_4k"
