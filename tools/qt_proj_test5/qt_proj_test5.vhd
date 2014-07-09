a1d004667339898a26a490080701604470392dc6
astoria-d <astoria-d@mail.goo.ne.jp>
2014-07-09 13:51:59 +0900

    clean up

--- tools/qt_proj_test5/qt_proj_test5.vhd
+++ tools/qt_proj_test5/qt_proj_test5.vhd
@@ -7,7 +7,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.conv_integer;
 
-entity qt_proj_test5_old is 
+entity qt_proj_test5 is 
     port (
 
     signal dbg_cpu_clk  : out std_logic;
@@ -15,18 +15,6 @@ entity qt_proj_test5_old is
     signal dbg_mem_clk  : out std_logic;
     signal dbg_addr : out std_logic_vector( 16 - 1 downto 0);
     signal dbg_d_io : out std_logic_vector( 8 - 1 downto 0);
-
---    signal dbg_status       : out std_logic_vector(7 downto 0);
---    signal dbg_dec_oe_n    : out std_logic;
---    signal dbg_dec_val     : out std_logic_vector (7 downto 0);
---    signal dbg_int_dbus    : out std_logic_vector (7 downto 0);
---    signal dbg_status_val    : out std_logic_vector (7 downto 0);
---    signal dbg_stat_we_n    : out std_logic;
-    
----monitor inside cpu
---    signal dbg_d1, dbg_d2, dbg_d_out: out std_logic_vector (7 downto 0);
---    signal dbg_ea_carry, dbg_carry_clr_n    : out std_logic;
---    signal dbg_gate_n    : out std_logic;
 
         signal dbg_pos_x       : out std_logic_vector (8 downto 0);
         signal dbg_pos_y       : out std_logic_vector (8 downto 0);
@@ -80,9 +68,9 @@ entity qt_proj_test5_old is
 		dram_we_n	:	out std_logic 							--Write Enable
 
         );
-end qt_proj_test5_old;
+end qt_proj_test5;
 
-architecture rtl of qt_proj_test5_old is
+architecture rtl of qt_proj_test5 is
 
     component clock_divider
         port (  base_clk    : in std_logic;
@@ -209,38 +197,6 @@ component sdram_controller
 end component;
 
 
-component sdram_rw
-  generic(
-		reset_polarity :	std_logic := '0' --When rst = reset_polarity, system at RESET
-	);
-  port(
-		--Clock and Reset
-		clk_i		:	in std_logic;	--WISHBONE Clock
-		rst			:	in std_logic;	--RESET
-		
-		--Signals to SDRAM controller
-		wbm_adr_o	:	out std_logic_vector (21 downto 0);	--Address to read from / write to
-		wbm_dat_i	:	in std_logic_vector (15 downto 0);	--Data out (to SDRAM)
-		wbm_dat_o	:	out std_logic_vector (15 downto 0);	--Data in (from SDRAM)
-		wbm_we_i	:	out std_logic;	--'1' - Write, '0' - Read
-		wbm_tga_o	:	out std_logic_vector (7 downto 0);	--Read/write burst length-1 (0 represents 1 word, FF represents 256 words)
-		wbm_cyc_o	:	out std_logic;	--Transmit command to SDRAM controller
-		wbm_stb_o	:	out std_logic;	--Transmit command to SDRAM controller
-		wbm_stall_i	:	in std_logic;	--When '1', write data to SDRAM
-		wbm_ack_i	:	in std_logic;	--when '1', data is ready to be read from SDRAM
-		
-		--Debug and test signals
-		cmd_ack		:	in std_logic;	--Command has been acknowledged by SDRAM controller
-		cmd_done	:	in std_logic;	--Command has finished (read/write)
-		green_led	:	out std_logic;	--Test passed
-		red_led		:	out std_logic;	--Test fail
-		writing		:	out std_logic;	--'1' when writing, '0' when reading
-		mem_val_o	:	out std_logic_vector (15 downto 0); --Memory value written / compared to SDRAM
-		sdram_val_o	:	out std_logic_vector (15 downto 0); --Read value from SDRAM
-		cur_st_o	:	out std_logic_vector (3 downto 0)	--Current state
-   );
-end component;
-
     constant data_size : integer := 8;
     constant addr_size : integer := 16;
     constant size14    : integer := 14;
@@ -328,76 +284,49 @@ begin
 --    end process;
 
     
---    vga_ctl_inst : vga_ctl
---    port map (  
---        dbg_vga_x        ,
---        dbg_vga_y        ,
---        dbg_nes_x        ,
---        dbg_nes_x_old    ,
---        dbg_sw_state     ,
---        
---        dbg_f_in             ,
---        dbg_f_out            ,
---        dbg_f_cnt            ,
---        dbg_f_rd, dbg_f_wr, dbg_f_emp, dbg_f_ful ,
---        dbg_bst_cnt          ,
---
---            ppu_clk     ,
---            --vga_clk_pll, 
---            --ppu_clk ,
---            vga_clk     ,
---            mem_clk     ,
---            rst_n       ,
---            pos_x       ,
---            pos_y       ,
---            nes_r       ,
---            nes_g       ,
---            nes_b       ,
---            h_sync_n    ,
---            v_sync_n    ,
---            r           ,
---            g           ,
---            b           ,
---            
---            --SDRAM Signals
---            wbs_adr_i	,
---            wbs_dat_i	,
---            wbs_we_i	,
---            wbs_tga_i	,
---            wbs_cyc_i	,
---            wbs_stb_i	,
---            wbs_dat_o	,
---            wbs_stall_o	,
---            wbs_err_o	,
---            wbs_ack_o	
---    );
-
-    test_sdram_inst : sdram_rw
+    vga_ctl_inst : vga_ctl
     port map (  
-		mem_clk,
-		rst_n,
-		
-		--Signals to SDRAM controller
-		wbs_adr_i,
-		wbs_dat_o,
-		wbs_dat_i,
-		wbs_we_i,
-		wbs_tga_i,
-		wbs_cyc_i,
-		wbs_stb_i,
-		wbs_stall_o,
-		wbs_ack_o,
-		
-		--Debug and test signals
-		cmd_ack		,
-		cmd_done	,
-		green_led	,
-		red_led		,
-		writing		,
-		mem_val_o	,
-		sdram_val_o	,
-		cur_st_o	
-   );
+        dbg_vga_x        ,
+        dbg_vga_y        ,
+        dbg_nes_x        ,
+        dbg_nes_x_old    ,
+        dbg_sw_state     ,
+        
+        dbg_f_in             ,
+        dbg_f_out            ,
+        dbg_f_cnt            ,
+        dbg_f_rd, dbg_f_wr, dbg_f_emp, dbg_f_ful ,
+        dbg_bst_cnt          ,
+
+            ppu_clk     ,
+            --vga_clk_pll, 
+            --ppu_clk ,
+            vga_clk     ,
+            mem_clk     ,
+            rst_n       ,
+            pos_x       ,
+            pos_y       ,
+            nes_r       ,
+            nes_g       ,
+            nes_b       ,
+            h_sync_n    ,
+            v_sync_n    ,
+            r           ,
+            g           ,
+            b           ,
+            
+            --SDRAM Signals
+            wbs_adr_i	,
+            wbs_dat_i	,
+            wbs_we_i	,
+            wbs_tga_i	,
+            wbs_cyc_i	,
+            wbs_stb_i	,
+            wbs_dat_o	,
+            wbs_stall_o	,
+            wbs_err_o	,
+            wbs_ack_o	
+    );
 
     dram_clk <= not mem_clk;
     sdram_clk <= not mem_clk;
@@ -439,248 +368,6 @@ sdram_ctl_inst : sdram_controller
 		main_st_o	
    ); 
         
-    --    signal addr : std_logic_vector( addr_size - 1 downto 0);
---    signal d_io : std_logic_vector( data_size - 1 downto 0);
---
---component counter_register
---    generic (
---        dsize       : integer := 8;
---        inc         : integer := 1
---    );
---    port (  clk         : in std_logic;
---            rst_n       : in std_logic;
---            ce_n        : in std_logic;
---            we_n        : in std_logic;
---            d           : in std_logic_vector(dsize - 1 downto 0);
---            q           : out std_logic_vector(dsize - 1 downto 0)
---    );
---end component;
---
---component prg_rom
---    generic (abus_size : integer := 15; dbus_size : integer := 8);
---    port (  clk             : in std_logic;
---            ce_n           : in std_logic;   --select pin active low.
---            addr            : in std_logic_vector (abus_size - 1 downto 0);
---            data            : inout std_logic_vector (dbus_size - 1 downto 0)
---        );
---end component;
---
---component processor_status 
---    generic (
---            dsize : integer := 8
---            );
---    port (  
---    signal dbg_dec_oe_n    : out std_logic;
---    signal dbg_dec_val     : out std_logic_vector (dsize - 1 downto 0);
---    signal dbg_int_dbus    : out std_logic_vector (dsize - 1 downto 0);
---    signal dbg_status_val    : out std_logic_vector (7 downto 0);
---    signal dbg_stat_we_n    : out std_logic;
---    
---            clk         : in std_logic;
---            res_n       : in std_logic;
---            dec_oe_n    : in std_logic;
---            bus_oe_n    : in std_logic;
---            set_flg_n   : in std_logic;
---            flg_val     : in std_logic;
---            load_bus_all_n      : in std_logic;
---            load_bus_nz_n       : in std_logic;
---            set_from_alu_n      : in std_logic;
---            alu_n       : in std_logic;
---            alu_v       : in std_logic;
---            alu_z       : in std_logic;
---            alu_c       : in std_logic;
---            stat_c      : out std_logic;
---            dec_val     : inout std_logic_vector (dsize - 1 downto 0);
---            int_dbus    : inout std_logic_vector (dsize - 1 downto 0)
---        );
---end component;
---
---    ---status register
---    signal status_reg, int_d_bus : std_logic_vector (7 downto 0);
---    signal stat_dec_oe_n : std_logic;
---    signal stat_bus_oe_n : std_logic;
---    signal stat_set_flg_n : std_logic;
---    signal stat_flg : std_logic;
---    signal stat_bus_all_n : std_logic;
---    signal stat_bus_nz_n : std_logic;
---    signal stat_alu_we_n : std_logic;
---    signal alu_n : std_logic;
---    signal alu_z : std_logic;
---    signal alu_c : std_logic;
---    signal alu_v : std_logic;
---    signal stat_c : std_logic;
---    signal trig_clk : std_logic;
---    
---    
---    
---    component alu_test
---    port (  
---        d1    : in std_logic_vector(7 downto 0);
---        d2    : in std_logic_vector(7 downto 0);
---        d_out    : out std_logic_vector(7 downto 0);
---        carry_clr_n : in std_logic;
---        ea_carry : out std_logic
---        );
---end component;
---
---    signal d1, d2, d_out : std_logic_vector (7 downto 0);
---    signal ea_carry, gate_n    : std_logic;
---        signal carry_clr_n : std_logic;
-
-
-    
-    
---    trig_clk <= not cpu_clk;
---
---    pcl_inst : counter_register generic map (16) port map
---        (cpu_clk, rst_n, '0', '1', (others => '0'), addr(15 downto 0));
---
---    rom_inst : prg_rom generic map (12, 8) port map
---        (base_clk, '0', addr(11 downto 0), d_io);
---
---    dbg_addr <= addr;
---    dbg_d_io <= d_io;
---
---    dbg_d1 <= d1;
---    dbg_d2 <= d2;
---    dbg_d_out <= d_out;
---    dbg_ea_carry <= ea_carry;
---    dbg_carry_clr_n <= carry_clr_n;
---    dbg_gate_n <= gate_n;
---    
---    dummy_alu : alu_test
---    port map (  
---        d1, d2, d_out, carry_clr_n , ea_carry
---        );
---
---        gate_n <= not ea_carry;
---    dec_test_p : process (rst_n, ea_carry, trig_clk)
---    begin
---        if (rst_n = '0') then
---            d1 <= "00000000";
---            d2 <= "00000000";
---            carry_clr_n <= '0';
---            --gate_n <= '1';
-----        elsif (ea_carry = '1') then
-----            gate_n <= '0';
-----            carry_clr_n <= '0';
---        elsif (rising_edge(trig_clk)) then
---            if (addr(5 downto 0) = "000001") then
---            --addr=01
---                carry_clr_n <= '1';
---                d1 <= "00010011";
---                d2 <= "01000111";
---                --gate_n <= '1';
---            elsif (addr(5 downto 0) = "000010") then
---            --addr=02
---                carry_clr_n <= '1';
---                d1 <= "00110011";
---                d2 <= "11001111";
---                --gate_n <= '1';
---            elsif (addr(5 downto 0) = "000011") then
---            --addr=03
---                carry_clr_n <= '1';
---                d1 <= "00001010";
---                d2 <= "01011001";
---                --gate_n <= '1';
---            elsif (addr(5 downto 0) = "000100") then
---            --addr=04
---                carry_clr_n <= '1';
---                d1 <= "10001010";
---                d2 <= "10011001";
---                --gate_n <= '1';
---            else
---                carry_clr_n <= '1';
---                d1 <= "00000000";
---                d2 <= "00000000";
---                --gate_n <= '1';
---            end if;
---        end if;
---    end process;
---
---
---    --status register
---    status_register : processor_status generic map (8) 
---            port map (
---    dbg_dec_oe_n,
---    dbg_dec_val,
---    dbg_int_dbus,
---    dbg_status_val,
---    dbg_stat_we_n    ,
---                    trig_clk , rst_n, 
---                    stat_dec_oe_n, stat_bus_oe_n, 
---                    stat_set_flg_n, stat_flg, stat_bus_all_n, stat_bus_nz_n, 
---                    stat_alu_we_n, alu_n, alu_v, alu_z, alu_c, stat_c,
---                    status_reg, int_d_bus);
---
---    dbg_status <= status_reg;
---    status_test_p : process (addr)
---    begin
---        if (addr(5 downto 0) = "000010") then
---        --addr=02
---        --set status(7) = '1'
---            stat_dec_oe_n <= '1';
---            stat_bus_oe_n <= '1';
---            stat_set_flg_n <= '0';
---            stat_flg <= '1';
---            stat_bus_all_n <= '1';
---            stat_bus_nz_n <= '1'; 
---            stat_alu_we_n <= '1';
---            status_reg <= "01000000";
---            int_d_bus <= "00000000";
---
---        elsif (addr(5 downto 0) = "000100") then
---        --addr=04
---        --set status(2) = '0'
---            stat_dec_oe_n <= '1';
---            stat_bus_oe_n <= '1';
---            stat_set_flg_n <= '0';
---            stat_flg <= '0';
---            stat_bus_all_n <= '1';
---            stat_bus_nz_n <= '1'; 
---            stat_alu_we_n <= '1';
---            status_reg <= "00000100";
---            int_d_bus <= "00000000";
---
---        elsif (addr(5 downto 0) = "000110") then
---        --addr=06
---        --set nz from bus, n=1
---            stat_dec_oe_n <= '1';
---            stat_bus_oe_n <= '1';
---            stat_set_flg_n <= '1';
---            stat_flg <= '0';
---            stat_bus_all_n <= '1';
---            stat_bus_nz_n <= '0'; 
---            stat_alu_we_n <= '1';
---            status_reg <= (others => 'Z');
---            int_d_bus <= "10000000";
---
---        elsif (addr(5 downto 0) = "001000") then
---        --addr=08
---        --set nz from bus, z=1
---            stat_dec_oe_n <= '1';
---            stat_bus_oe_n <= '1';
---            stat_set_flg_n <= '1';
---            stat_flg <= '0';
---            stat_bus_all_n <= '1';
---            stat_bus_nz_n <= '0'; 
---            stat_alu_we_n <= '1';
---            status_reg <= (others => 'Z');
---            int_d_bus <= "00000000";
---
---        else
---            stat_dec_oe_n <= '0';
---            stat_bus_oe_n <= '1';
---            stat_set_flg_n <= '1';
---            stat_flg <= '1';
---            stat_bus_all_n <= '1';
---            stat_bus_nz_n <= '1'; 
---            stat_alu_we_n <= '1';
---            status_reg <= (others => 'Z');
---            int_d_bus <= (others => 'Z');
---        end if;
---    end process;
-
 end rtl;
 
 
@@ -702,7 +389,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.conv_integer;
 
-entity qt_proj_test5 is 
+entity sdram_test is 
     port (
 
     signal dbg_cpu_clk  : out std_logic;
@@ -763,9 +450,9 @@ entity qt_proj_test5 is
 		dram_we_n	:	out std_logic 							--Write Enable
 
         );
-end qt_proj_test5;
+end sdram_test;
 
-architecture rtl of qt_proj_test5 is
+architecture rtl of sdram_test is
 
 component sdram_controller 
   generic
