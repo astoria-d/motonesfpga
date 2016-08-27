f11ab5dd293984f5bc73aadc15cb5256ddab6a89
astoria-d <astoria-d@mail.goo.ne.jp>
2016-08-27 22:57:20 +0900

    de0-cv latest merge

--- de0_cv_nes/de0_cv_nes.vhd
+++ de0_cv_nes/de0_cv_nes.vhd
@@ -12,10 +12,13 @@ entity de0_cv_nes is
 --debug signal
     signal dbg_cpu_clk  : out std_logic;
     signal dbg_ppu_clk  : out std_logic;
-    signal dbg_mem_clk  : out std_logic;
+    signal dbg_emu_ppu_clk  : out std_logic;
+    signal dbg_cpu_mem_clk  : out std_logic;
     signal dbg_r_nw     : out std_logic;
     signal dbg_addr     : out std_logic_vector( 16 - 1 downto 0);
     signal dbg_d_io     : out std_logic_vector( 8 - 1 downto 0);
+    signal dbg_v_addr   : out std_logic_vector (13 downto 0);
+    signal dbg_v_data   : out std_logic_vector (7 downto 0);
 
 ---monitor inside cpu
     signal dbg_instruction  : out std_logic_vector(7 downto 0);
@@ -54,28 +57,23 @@ architecture rtl of de0_cv_nes is
                 );
         port (  
     signal dbg_instruction  : out std_logic_vector(7 downto 0);
-    signal dbg_int_d_bus    : out std_logic_vector(7 downto 0);
+    signal dbg_int_d_bus  : out std_logic_vector(7 downto 0);
     signal dbg_exec_cycle      : out std_logic_vector (5 downto 0);
     signal dbg_ea_carry     : out std_logic;
---    signal dbg_index_bus    : out std_logic_vector(7 downto 0);
---    signal dbg_acc_bus      : out std_logic_vector(7 downto 0);
     signal dbg_status       : out std_logic_vector(7 downto 0);
     signal dbg_pcl, dbg_pch, dbg_sp, dbg_x, dbg_y, dbg_acc       : out std_logic_vector(7 downto 0);
     signal dbg_dec_oe_n    : out std_logic;
     signal dbg_dec_val     : out std_logic_vector (7 downto 0);
---    signal dbg_status_val    : out std_logic_vector (7 downto 0);
     signal dbg_stat_we_n    : out std_logic;
     signal dbg_idl_h, dbg_idl_l     : out std_logic_vector (7 downto 0);
-    
-                input_clk   : in std_logic; --phi0 input pin.
+
+                cpu_clk     : in std_logic; --phi0 input pin.
+                dl_cpu_clk  : in std_logic; --phi1 delayed clock.
                 rdy         : in std_logic;
                 rst_n       : in std_logic;
                 irq_n       : in std_logic;
                 nmi_n       : in std_logic;
-                dbe         : in std_logic;
                 r_nw        : out std_logic;
-                phi1        : out std_logic;
-                phi2        : out std_logic;
                 addr        : out std_logic_vector ( asize - 1 downto 0);
                 d_io        : inout std_logic_vector ( dsize - 1 downto 0)
         );
@@ -86,16 +84,17 @@ architecture rtl of de0_cv_nes is
                 reset_n     : in std_logic;
                 cpu_clk     : out std_logic;
                 ppu_clk     : out std_logic;
-                mem_clk     : out std_logic;
-                vga_clk     : out std_logic
+                emu_ppu_clk : out std_logic;
+                vga_clk     : out std_logic;
+                cpu_mem_clk     : out std_logic;
+                cpu_recv_clk     : out std_logic;
+                emu_ppu_mem_clk : out std_logic
             );
     end component;
 
     component address_decoder
     generic (abus_size : integer := 16; dbus_size : integer := 8);
-        port (  phi2        : in std_logic;
-                mem_clk     : in std_logic;
-                R_nW        : in std_logic; 
+        port (
                 addr        : in std_logic_vector (abus_size - 1 downto 0);
                 rom_ce_n    : out std_logic;
                 ram_ce_n    : out std_logic;
@@ -130,8 +129,6 @@ architecture rtl of de0_cv_nes is
         signal dbg_ppu_addr : out std_logic_vector (13 downto 0);
         signal dbg_ppu_data, dbg_ppu_scrl_x, dbg_ppu_scrl_y : out std_logic_vector (7 downto 0);
 
-        signal dbg_ppu_clk                      : out std_logic;
-        signal dbg_vga_clk                      : out std_logic;
         signal dbg_nes_x                        : out std_logic_vector (8 downto 0);
         signal dbg_vga_x                        : out std_logic_vector (9 downto 0);
         signal dbg_nes_y                        : out std_logic_vector (8 downto 0);
@@ -147,13 +144,11 @@ architecture rtl of de0_cv_nes is
         signal dbg_s_oam_ce_rn_wn               : out std_logic_vector (2 downto 0);
         signal dbg_s_oam_addr                   : out std_logic_vector (4 downto 0);
         signal dbg_s_oam_data                   : out std_logic_vector (7 downto 0);
-        signal dbg_emu_ppu_clk                  : out std_logic;
 
-        signal dbg_ppu_addr_we_n                : out std_logic;
-        signal dbg_ppu_clk_cnt                  : out std_logic_vector(1 downto 0);
-
+                dl_cpu_clk  : in std_logic;
                 ppu_clk     : in std_logic;
-                mem_clk     : in std_logic;
+                vga_clk     : in std_logic;
+                emu_ppu_clk : in std_logic;
                 ce_n        : in std_logic;
                 rst_n       : in std_logic;
                 r_nw        : in std_logic;
@@ -163,11 +158,10 @@ architecture rtl of de0_cv_nes is
                 vblank_n    : out std_logic;
                 rd_n        : out std_logic;
                 wr_n        : out std_logic;
-                ale         : out std_logic;
-                vram_ad     : inout std_logic_vector (7 downto 0);
-                vram_a      : out std_logic_vector (13 downto 8);
+                ale_n       : out std_logic;
+                vram_addr   : out std_logic_vector (13 downto 0);
+                vram_data   : inout std_logic_vector (7 downto 0);
 
-                vga_clk     : in std_logic;
                 h_sync_n    : out std_logic;
                 v_sync_n    : out std_logic;
                 r           : out std_logic_vector(3 downto 0);
@@ -178,13 +172,8 @@ architecture rtl of de0_cv_nes is
 
     component v_address_decoder
     generic (abus_size : integer := 14; dbus_size : integer := 8);
-        port (  clk         : in std_logic; 
-                mem_clk     : in std_logic;
-                rd_n        : in std_logic;
-                wr_n        : in std_logic;
-                ale         : in std_logic;
+        port (
                 v_addr      : in std_logic_vector (13 downto 0);
-                v_data      : in std_logic_vector (7 downto 0);
                 nt_v_mirror : in std_logic;
                 pt_ce_n     : out std_logic;
                 nt0_ce_n    : out std_logic;
@@ -202,16 +191,17 @@ architecture rtl of de0_cv_nes is
         );
     end component;
 
-    component ls373
+    component d_flip_flop
         generic (
-            dsize : integer := 8
-        );
-        port (  c         : in std_logic;
-                we_n      : in std_logic;
-                oc_n      : in std_logic;
-                d         : in std_logic_vector(dsize - 1 downto 0);
-                q         : out std_logic_vector(dsize - 1 downto 0)
-        );
+                dsize : integer := 8
+                );
+        port (  clk     : in std_logic;
+                res_n   : in std_logic;
+                set_n   : in std_logic;
+                we_n    : in std_logic;
+                d       : in std_logic_vector (dsize - 1 downto 0);
+                q       : out std_logic_vector (dsize - 1 downto 0)
+            );
     end component;
 
     component apu
@@ -237,11 +227,13 @@ architecture rtl of de0_cv_nes is
 
     signal cpu_clk  : std_logic;
     signal ppu_clk  : std_logic;
-    signal mem_clk  : std_logic;
-    signal vga_clk   : std_logic;
+    signal vga_clk  : std_logic;
+    signal emu_ppu_clk  : std_logic;
+    signal cpu_mem_clk     : std_logic;
+    signal cpu_recv_clk    : std_logic;
+    signal emu_ppu_mem_clk : std_logic;
 
-    signal rdy, irq_n, nmi_n, dbe, r_nw : std_logic;
-    signal phi1, phi2 : std_logic;
+    signal rdy, irq_n, nmi_n, r_nw : std_logic;
     signal addr : std_logic_vector( addr_size - 1 downto 0);
     signal d_io : std_logic_vector( data_size - 1 downto 0);
 
@@ -253,30 +245,21 @@ architecture rtl of de0_cv_nes is
 
     signal rd_n     : std_logic;
     signal wr_n     : std_logic;
-    signal ale      : std_logic;
-    signal vram_ad  : std_logic_vector (7 downto 0);
-    signal vram_a   : std_logic_vector (13 downto 8);
+    signal ale_n    : std_logic;
     signal v_addr   : std_logic_vector (13 downto 0);
+    signal v_addr_ppu   : std_logic_vector (13 downto 0);
+    signal v_data   : std_logic_vector (7 downto 0);
     signal pt_ce_n  : std_logic;
     signal nt0_ce_n : std_logic;
     signal nt1_ce_n : std_logic;
 
-    signal ale_n       : std_logic;
-
-    signal dbg_int_d_bus                    : std_logic_vector(7 downto 0);
-    signal dbg_sp, dbg_x, dbg_y, dbg_acc    : std_logic_vector(7 downto 0);
-    signal dbg_dec_oe_n                     : std_logic;
-    signal dbg_disp_nt, dbg_disp_attr       : std_logic_vector (7 downto 0);
-    signal dbg_vram_ad                      : std_logic_vector (7 downto 0);
-    signal dbg_vram_a                       : std_logic_vector (13 downto 8);
-    signal dbg_disp_ptn_h, dbg_disp_ptn_l : std_logic_vector (15 downto 0);
+--    signal dbg_disp_nt, dbg_disp_attr : std_logic_vector (7 downto 0);
+--    signal dbg_disp_ptn_h, dbg_disp_ptn_l : std_logic_vector (15 downto 0);
     signal dbg_pcl, dbg_pch : std_logic_vector(7 downto 0);
     signal dbg_stat_we_n    : std_logic;
     signal dbg_idl_h, dbg_idl_l     : std_logic_vector (7 downto 0);
 
     signal dbg_vga_clk                      : std_logic;
-    signal dbg_ppu_addr_we_n                : std_logic;
-    signal dbg_ppu_clk_cnt                  : std_logic_vector(1 downto 0);
     signal dbg_ppu_addr_dummy               : std_logic_vector (13 downto 0);
     signal dbg_nes_x                        : std_logic_vector (8 downto 0);
     signal dbg_vga_x                        : std_logic_vector (9 downto 0);
@@ -291,7 +274,6 @@ architecture rtl of de0_cv_nes is
     signal dbg_s_oam_ce_rn_wn               : std_logic_vector (2 downto 0);
     signal dbg_s_oam_addr                   : std_logic_vector (4 downto 0);
     signal dbg_s_oam_data                   : std_logic_vector (7 downto 0);
-    signal dbg_emu_ppu_clk                  : std_logic;
     signal dbg_ppu_data_dummy               : std_logic_vector (7 downto 0);
     signal dbg_ppu_status_dummy             : std_logic_vector (7 downto 0);
     signal dbg_ppu_scrl_x_dummy             : std_logic_vector (7 downto 0);
@@ -299,11 +281,17 @@ architecture rtl of de0_cv_nes is
     signal dbg_disp_ptn_h_dummy, dbg_disp_ptn_l_dummy   : std_logic_vector (15 downto 0);
 
     signal dbg_instruction_dummy  : std_logic_vector(7 downto 0);
-    signal dbg_dec_val            : std_logic_vector(7 downto 0);
+    signal dbg_int_d_bus_dummy    : std_logic_vector(7 downto 0);
     signal dbg_exec_cycle_dummy   : std_logic_vector (5 downto 0);
     signal dbg_ea_carry_dummy     : std_logic;
     signal dbg_status_dummy       : std_logic_vector(7 downto 0);
     signal dbg_sp_dummy, dbg_x_dummy, dbg_y_dummy, dbg_acc_dummy       : std_logic_vector(7 downto 0);
+    signal dbg_dec_oe_n    : std_logic;
+    signal dbg_dec_val     : std_logic_vector (7 downto 0);
+    signal dbg_int_d_bus    : std_logic_vector(7 downto 0);
+    signal dbg_sp, dbg_x, dbg_y, dbg_acc       : std_logic_vector(7 downto 0);
+    signal dbg_disp_nt, dbg_disp_attr : std_logic_vector (7 downto 0);
+    signal dbg_disp_ptn_h, dbg_disp_ptn_l : std_logic_vector (15 downto 0);
 
 
     component counter_register 
@@ -328,39 +316,36 @@ begin
 
     --ppu/cpu clock generator
     clock_inst : clock_divider port map 
-        (base_clk, rst_n, cpu_clk, ppu_clk, mem_clk, vga_clk);
+        (base_clk, rst_n, cpu_clk, ppu_clk, emu_ppu_clk, vga_clk, cpu_mem_clk, cpu_recv_clk, emu_ppu_mem_clk);
 
     addr_dec_inst : address_decoder generic map (addr_size, data_size) 
-        port map (phi2, mem_clk, r_nw, addr, rom_ce_n, ram_ce_n, ppu_ce_n, apu_ce_n);
+        port map (addr, rom_ce_n, ram_ce_n, ppu_ce_n, apu_ce_n);
 
     --mos 6502 cpu instance
     cpu_inst : mos6502 generic map (data_size, addr_size) 
         port map (
-    dbg_instruction,
-    dbg_int_d_bus,
-    dbg_exec_cycle,
+    dbg_instruction_dummy,
+    dbg_int_d_bus_dummy,
+    dbg_exec_cycle_dummy,
     dbg_ea_carry,
- --   dbg_index_bus,
- --   dbg_acc_bus,
     dbg_status_dummy,
     dbg_pcl, dbg_pch, dbg_sp_dummy, dbg_x_dummy, dbg_y, dbg_acc,
     dbg_dec_oe_n,
     dbg_dec_val,
---    dbg_status_val    ,
     dbg_stat_we_n    ,
     dbg_idl_h, dbg_idl_l,
 
-                cpu_clk, rdy,
-                rst_n, irq_n, nmi_n, dbe, r_nw, 
-                phi1, phi2, addr, d_io);
+                cpu_clk, cpu_recv_clk, rdy,
+                rst_n, irq_n, nmi_n, r_nw, 
+                addr, d_io);
 
     --main ROM/RAM instance
     prg_rom_inst : prg_rom generic map (rom_32k, data_size)
-            port map (mem_clk, rom_ce_n, addr(rom_32k - 1 downto 0), d_io);
+            port map (cpu_mem_clk, rom_ce_n, addr(rom_32k - 1 downto 0), d_io);
 
     ram_oe_n <= not R_nW;
     prg_ram_inst : ram generic map (ram_2k, data_size)
-            port map (mem_clk, ram_ce_n, ram_oe_n, R_nW, addr(ram_2k - 1 downto 0), d_io);
+            port map (cpu_mem_clk, ram_ce_n, ram_oe_n, R_nW, addr(ram_2k - 1 downto 0), d_io);
 
     --nes ppu instance
     ppu_inst: ppu port map (  
@@ -369,8 +354,6 @@ begin
         dbg_ppu_addr                                        ,
         dbg_ppu_data, dbg_ppu_scrl_x_dummy, dbg_ppu_scrl_y_dummy        ,
 
-        dbg_ppu_clk                      ,
-        dbg_vga_clk                      ,
         dbg_nes_x                        ,
         dbg_vga_x                        ,
         dbg_nes_y                        ,
@@ -386,12 +369,11 @@ begin
         dbg_s_oam_ce_rn_wn              ,
         dbg_s_oam_addr                  ,
         dbg_s_oam_data                  ,
-        dbg_emu_ppu_clk                 ,
-        dbg_ppu_addr_we_n                                   ,
-        dbg_ppu_clk_cnt                                     ,
 
+                cpu_mem_clk     ,
                 ppu_clk         ,
-                mem_clk     ,
+                vga_clk     ,
+                emu_ppu_clk     ,
                 ppu_ce_n        ,
                 rst_n       ,
                 r_nw        ,
@@ -401,11 +383,10 @@ begin
                 nmi_n    ,
                 rd_n        ,
                 wr_n        ,
-                ale         ,
-                vram_ad     ,
-                vram_a      ,
+                ale_n       ,
+                v_addr_ppu      ,
+                v_data      ,
 
-                vga_clk     ,
                 h_sync_n    ,
                 v_sync_n    ,
                 r           ,
@@ -415,39 +396,35 @@ begin
         );
 
     ppu_addr_decoder : v_address_decoder generic map (vram_size14, data_size) 
-        port map (ppu_clk, mem_clk, rd_n, wr_n, ale, v_addr, vram_ad, 
-                nt_v_mirror, pt_ce_n, nt0_ce_n, nt1_ce_n);
-
-    ---VRAM/CHR ROM instances
-    v_addr (13 downto 8) <= vram_a;
+        port map (v_addr, nt_v_mirror, pt_ce_n, nt0_ce_n, nt1_ce_n);
 
     --transparent d-latch
-    --ale=1 >> addr latch
-    --ale=0 >> addr output.
-	ale_n <= not ale;
-	vram_latch : ls373 generic map (data_size)
-                port map(vga_clk, ale_n, ale, vram_ad, v_addr(7 downto 0));
+    --ale_n=0 >> addr latch
+    --ale_n=1 >> addr output.
+	vram_latch : d_flip_flop generic map (vram_size14)
+                port map(emu_ppu_mem_clk, rst_n, '1', ale_n, v_addr_ppu, v_addr);
 
     vchr_rom : chr_rom generic map (chr_rom_8k, data_size)
-            port map (mem_clk, pt_ce_n, v_addr(chr_rom_8k - 1 downto 0), vram_ad);
+            port map (emu_ppu_mem_clk, pt_ce_n, v_addr(chr_rom_8k - 1 downto 0), v_data);
 
     --name table/attr table
     vram_nt0 : ram generic map (vram_1k, data_size)
-            port map (mem_clk, nt0_ce_n, rd_n, wr_n, v_addr(vram_1k - 1 downto 0), vram_ad);
+            port map (emu_ppu_mem_clk, nt0_ce_n, rd_n, wr_n, v_addr(vram_1k - 1 downto 0), v_data);
 
     vram_nt1 : ram generic map (vram_1k, data_size)
-            port map (mem_clk, nt1_ce_n, rd_n, wr_n, v_addr(vram_1k - 1 downto 0), vram_ad);
+            port map (emu_ppu_mem_clk, nt1_ce_n, rd_n, wr_n, v_addr(vram_1k - 1 downto 0), v_data);
 
     --APU/DMA instance
     apu_inst : apu
         port map (cpu_clk, apu_ce_n, rst_n, r_nw, addr, d_io, rdy);
+
+
 
 -----------------------------------------------------------
 -----------------------------------------------------------
 ------------------debug pin setting....--------------------    
 -----------------------------------------------------------
 -----------------------------------------------------------
-
     clock_counter_inst : counter_register generic map (64) port map 
         (cpu_clk, rst_n, '0', '1', (others=>'0'), clock_counter);
 
@@ -458,45 +435,43 @@ begin
 --    dbg_mem_clk <= loop24(21);
 
 
-    dbg_base_clk <= ppu_clk;
---    dbg_exec_cycle(2 downto 1) <= dbg_vga_x(9 downto 8);
---    dbg_exec_cycle(0) <= dbg_nes_x(8);
---    dbg_instruction <= dbg_nes_x(7 downto 0);
---    dbg_exec_cycle(3) <= dbg_emu_ppu_clk;
---
---    dbg_exec_cycle(4) <= dbg_nes_y(8);
---    dbg_status <= dbg_nes_y(7 downto 0);
+    --ppu debug....
+    dbg_exec_cycle(0) <= dbg_nes_x(8);
+    dbg_instruction <= dbg_nes_x(7 downto 0);
+    dbg_exec_cycle(4) <= dbg_nes_y(8);
+    dbg_status <= dbg_nes_y(7 downto 0);
 
-
---    dbg_ppu_scrl_x(0) <= ale;
---    dbg_ppu_scrl_x(1) <= rd_n;
---    dbg_ppu_scrl_x(2) <= wr_n;
---    dbg_ppu_scrl_x(3) <= nt0_ce_n;
---    dbg_ppu_scrl_x(4) <= vga_clk;
---    dbg_ppu_scrl_x(5) <= rom_ce_n;
---    dbg_ppu_scrl_x(6) <= ram_ce_n;
---    dbg_ppu_scrl_x(7) <= addr(15);
---    dbg_ppu_scrl_y(2 downto 0) <= dbg_p_oam_ce_rn_wn(2 downto 0);
---    dbg_ppu_scrl_y(5 downto 3) <= dbg_plt_ce_rn_wn(2 downto 0);
     dbg_disp_ptn_l (7 downto 0) <= dbg_p_oam_addr;
     dbg_disp_ptn_l (15 downto 8) <= dbg_p_oam_data;
+    dbg_int_d_bus(4 downto 0) <= dbg_s_oam_addr(4 downto 0);
+    --dbg_ppu_scrl_y <= dbg_s_oam_data;
+    dbg_ppu_scrl_y <= dbg_ppu_scrl_y_dummy;
 
+    dbg_ppu_scrl_x(0) <= ale_n;
+    dbg_ppu_scrl_x(1) <= rd_n;
+    dbg_ppu_scrl_x(2) <= wr_n;
+    dbg_ppu_scrl_x(3) <= nt0_ce_n;
+
+    --nmi_n <= dummy_nmi;
+    ---------------
+
+    ----general...
     dbg_cpu_clk <= cpu_clk;
-    dbg_mem_clk <= mem_clk;
+    dbg_ppu_clk <= ppu_clk;
+    dbg_emu_ppu_clk <= emu_ppu_clk;
+    dbg_cpu_mem_clk <= cpu_mem_clk;
+    dbg_vga_clk <= vga_clk;
     dbg_r_nw <= r_nw;
     dbg_addr <= addr;
     dbg_d_io <= d_io;
-    dbg_vram_ad  <= vram_ad ;
-    dbg_vram_a  <= vram_a ;
-
-    dbg_sp(7 downto 6) <= dbg_ppu_clk_cnt;
-    dbg_sp(5 downto 0) <= v_addr (13 downto 8);
-    dbg_x <= v_addr (7 downto 0);
-
+    dbg_v_addr <= v_addr;
+    dbg_v_data <= v_data;
     dbg_nmi <= nmi_n;
---    nmi_n <= dummy_nmi;
-    dbg_ppu_scrl_x <= dbg_pcl;
-    dbg_ppu_scrl_y <= dbg_pch;
+
+    ----cpu...
+--    dbg_ppu_ctrl <= dbg_pcl;
+--    dbg_ppu_data <= dbg_idl_l;
+--    dbg_ppu_mask <= dbg_idl_h;
 
 end rtl;
 
